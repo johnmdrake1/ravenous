3e75848027a2fa7f451ced01b454e65fce102b4e
johnmdrake1 <32374869+johnmdrake1@users.noreply.github.com>
2024-09-26 03:46:41 -0500

    added styling for BusinessList component

--- src/components/BusinessList.js
+++ src/components/BusinessList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Business from './Business.js';
+import styles from './BusinessList.module.css';
 
 
 function BusinessList(){
     //for now just returning the business component a few times and not yet making use of map, an array, etc.
     return (
-        <div>
+        <div className={styles.BusinessList}>
             <Business />
             <Business />
             <Business />
