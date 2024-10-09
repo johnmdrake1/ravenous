b7adbe9e66e4f6af85b1db42677e59691df3507d
johnmdrake1 <32374869+johnmdrake1@users.noreply.github.com>
2024-10-09 00:03:52 -0500

    Added click handler that will set the currently selected sort option state variable to the button that was clicked. the button rendered by producesortoptions will have a className of styles.active if it matches or be empty string/default otherwise. onClick of button calls click handler. button now has value attribute that is also the API call/currently selected button value.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -14,15 +14,26 @@ function SearchBar(){
     const [searchLocaton, setSearchLocation] = useState('');
     //managing state for the selected sorting option to use, changed with the sort option buttons(Best Match, Highest Rated, Most Reviewed)
     const [searchSortOption, setSearchSortOption] = useState();
+    //click handler to change active sort option that will be the onclick attribute of each button element
+    function handleSortOptionClick(event){
+        //get the value attribute of the button that clicked
+        const selectedOption = event.target.value;
+        // call the state setter with this value to set the state to the value of the button that was clicked(which also happens to be the API call)
+        setSearchSortOption(selectedOption);
+    }
     // function to make sort option buttons
     function produceSortOptions(){
         //uses map to iterate over sortOptions keys(sortType)
         return Object.keys(sortOptions).map(sortType => {
             //sortCall is the corresponding value(API call) to each key in sortOptions
-            let sortCall = sortOptions[sortType];
+            const sortCall = sortOptions[sortType];
+            //boolean to determine wether this button is the option currently selected in the searchSortOption state
+            const isSelected = sortCall === searchSortOption;
             //return element with key being the value(API call) for the element of sortOptions and the displayed part being the key(type of sort desired) from sortOptions
             //using key may not be strictly necessary here. example code did <li> elements here but I'm doing buttons.
-            return <button key={sortCall}>{sortType}</button>;
+            //also setting value attribute to sortCall(API call) so it can be used in click handler handleSortOption(event)
+            //className is dynamically set to active if the current button(sortCall) is the option selected in the state variable or the empty string otherwise.
+            return <button className={isSelected ? styles.active : ''} onClick={handleSortOptionClick} value={sortCall} key={sortCall}>{sortType}</button>;
         });
     }
 
