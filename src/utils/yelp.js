25e716831be60a76a40eca85ee62989701ba3bea
johnmdrake1 <32374869+johnmdrake1@users.noreply.github.com>
2024-10-12 01:57:38 -0500

    Added .env file locally to store yelp api key i just generated, referenced in a constant at top of yelp.js. added .env to .gitignore. .env is in root directory. Create React App was used for this project so I shouldn't need any additional configuration for now.

--- src/utils/yelp.js
+++ src/utils/yelp.js
@@ -1,1 +1,4 @@
 //JavaScript Code for interacting with the Yelp API for Part 4 of the Ravenous Project
+
+//yelp api key stored in local .env file not committed to changes because ot .gitignore file
+const yelpKey = process.env.REACT_APP_YELP_API_KEY;
