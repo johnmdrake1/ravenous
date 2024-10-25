01dfebe655a475a7cf4d856b689bbce4e06f9d8a
johnmdrake1 <32374869+johnmdrake1@users.noreply.github.com>
2024-10-24 22:25:15 -0500

    App.js refactored to remove hardcoded list. Asynchronous function added within the function component in App.js that calls the yelp API search function defined in yelp.js and awaits its result. This function is given to the searchbar component as a prop. SearchBar.js has not yet been modified to use this, however. React and useState are now imported in App.js.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,58 @@
 import logo from './logo.svg';
 // import './App.css';
+import React, {useState} from 'react'
 import styles from './App.module.css';
-//import list of decoy businesses, this is just here now to test
 import BusinessList from './components/BusinessList';
 import SearchBar from './components/SearchBar';
-//mock business
-const bus = {
-  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-}
-//array for holding mock businesses, it will hold the same mock business multiple times for now
-const businesslist = [];
-//adding mock business multiple times
-businesslist.push(bus);
-businesslist.push(bus);
-businesslist.push(bus);
-businesslist.push(bus);
-businesslist.push(bus);
-businesslist.push(bus);
-businesslist.push(bus);
-businesslist.push(bus);
-businesslist.push(bus);
-businesslist.push(bus);
-businesslist.push(bus);
+import yelp from './utils/yelp.js';
+
+//previously used mock data, will now use actual api call result
+// //mock business
+// const bus = {
+//   imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+//   name: 'MarginOtto Pizzeria',
+//   address: '1010 Paddington Way',
+//   city: 'Flavortown',
+//   state: 'NY',
+//   zipCode: '10101',
+//   category: 'Italian',
+//   rating: 4.5,
+//   reviewCount: 90
+// }
+// //array for holding mock businesses, it will hold the same mock business multiple times for now
+// const businesslist = [];
+// //adding mock business multiple times
+// businesslist.push(bus);
+// businesslist.push(bus);
+// businesslist.push(bus);
+// businesslist.push(bus);
+// businesslist.push(bus);
+// businesslist.push(bus);
+// businesslist.push(bus);
+// businesslist.push(bus);
+// businesslist.push(bus);
+// businesslist.push(bus);
+// businesslist.push(bus);
 
 function App() {
+  //list of businesses will be stored in state variable businesslist, setter setBusinesslist will be called when the API is called
+  const [businesslist, setBusinesslist] = useState([]);
+  //asynchronous function for calling the yelp API search function, will be passed to SearchBar component to be called onclick
+  async function doSearch(term, location, sortBy){
+    try {
+      //await the call to the yelp api function and set it to a constant, businesses
+      const businesses = await yelp(term, location, sortBy);
+      //use the state setter to set it to this list of businesses
+      setBusinesslist(businesses);
+    } catch(error) {
+      console.error('Error fetching Yelp Data', error);
+    }
+  }
   return (
     <div className={styles.App}>
       {/* This is just here now and the default code commented out to test */}
       <h1>ravenous</h1>
-      <SearchBar />
+      <SearchBar doSearch={doSearch} />
       <BusinessList businesslist={businesslist} />
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
--- src/components/BusinessList.js
+++ src/components/BusinessList.js
@@ -8,7 +8,7 @@ function BusinessList(props){
     return (
         <div className={styles.BusinessList}>
             {/* map function goes through passed in business list prop, returns a Business instance for each business, passing each business in as a prop called bus with a value of
-            business from the map function */}
+            business from the map function, and a key of business.id */}
             {props.businesslist.map((business) => (<Business bus={business} key={business.id} />))}
             {/* {
             Old code from part 1
